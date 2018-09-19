1bb9c1de82e0a6d6ca6a9551c5df577a03bf26b2
coast <Coastchb@sina.com>
2018-09-19 21:32:51 +0800

    ch6/1 modified

--- TF_ML_codebook/ch6/1_gatas_activation.py
+++ TF_ML_codebook/ch6/1_gatas_activation.py
@@ -2,37 +2,90 @@
 # @Time		:2018/9/19 上午9:03
 # @Author	:Coast Cao
 
+import os
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 iter_num = 100;
 batch_size = 20;
+tf.set_random_seed(5)
+np.random.seed(42)
 
 x_vals = np.random.normal(2, 0.1, 500)
 t_vals = np.repeat(0.75, 500)
-batch_num = int(100 / batch_size)
+batch_num = int(500 / batch_size)
 samples = [[[[x_vals[j]] for j in range(i*batch_size,(i+1)*batch_size)], [[t_vals[j]] for j in range(i*batch_size,(i+1)*batch_size)]]
            for i in range(batch_num)]
 
 def create_model(activation, xp, yp):
-    W = tf.Variable(tf.random_normal(shape=[1,1],mean=10., stddev=0.1, dtype=tf.float32))
-    output = activation(tf.matmul(W, tf.transpose(xp)))
+    W = tf.Variable(tf.random_normal(shape=[1,1]),name="weight")
+    b = tf.Variable(tf.random_uniform(shape=[1,1]))
+    output = activation(tf.add(tf.matmul(W, tf.transpose(xp)), b))
     loss = tf.reduce_mean(tf.square(tf.subtract(tf.transpose(output), yp)))
     train_op = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
-    return loss, train_op
+    return W, output, loss, train_op
 
 xp = tf.placeholder(tf.float32, [None,1])
 yp = tf.placeholder(tf.float32, [None,1])
 
-re_loss, re_train = create_model(tf.nn.relu, xp, yp)
+with tf.variable_scope("relu") as scope:
+  re_W, re_output, re_loss, re_train = create_model(tf.nn.relu, xp, yp)
+with tf.variable_scope("sigmoid") as scope:
+  sg_W, sg_output, sg_loss, sg_train = create_model(tf.sigmoid, xp, yp)
 
 sess = tf.Session()
+summary_writer = tf.summary.FileWriter('tensorboard',
+                                      tf.get_default_graph())
+if not os.path.exists('tensorboard'):
+  os.makedirs('tensorboard')
 
 sess.run(tf.global_variables_initializer())
+
+re_Ws = []
+sg_Ws = []
+re_outputs = []
+sg_outputs = []
+re_losses = []
+sg_losses = []
 
 for i in range(iter_num):
     np.random.shuffle(samples)
     for j in range(batch_num):
-        sess.run(re_train, feed_dict={xp:samples[j][0], yp: samples[j][1]})
+        [[s_w]], s_output, s_loss, _ = sess.run([sg_W, sg_output, sg_loss, sg_train],
+                                                feed_dict={xp: samples[j][0], yp: samples[j][1]})
+        [[r_w]], r_output, r_loss, _ = sess.run([re_W, re_output, re_loss, re_train],
+                                                feed_dict={xp:samples[j][0], yp: samples[j][1]})
+        re_Ws.append(r_w)
+        sg_Ws.append(s_w)
+        re_outputs.append(np.mean(r_output))
+        sg_outputs.append(np.mean(s_output))
+        re_losses.append(r_loss)
+        sg_losses.append(s_loss)
+        print("re_w: %.3f" % r_w)
+        print("sg_w: %.3f" % s_w)
 
+plt.title("weights")
+plt.xlabel("iter")
+plt.ylabel("weight value")
+plt.plot(re_Ws, 'r--', label="relu weights")
+plt.plot(sg_Ws, 'b-', label="sigmoid weights")
+plt.legend(loc='upper right')
+plt.show()
 
+plt.title("loss")
+plt.xlabel("iter")
+plt.ylabel("loss value")
+plt.plot(re_losses, 'r--', label="relu loss")
+plt.plot(sg_losses, 'b-', label="sigmoid loss")
+plt.legend(loc='upper right')
+plt.show()
+
+plt.title("output")
+plt.xlabel("iter")
+plt.ylabel("output value")
+plt.plot(re_outputs, 'r--', label="relu output")
+plt.plot(sg_outputs, 'b-', label="sigmoid output")
+plt.ylim([0.0, 2.0] )
+plt.legend(loc='upper right')
+plt.show()
--- variable.py
+++ variable.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 v = tf.get_variable("v", shape=(), initializer=tf.zeros_initializer());
 w = v + 1;
@@ -6,6 +7,7 @@ print(v);
 print(w);
 
 def conv_relu(input, kernel_shape, bias_shape):
+
     weights = tf.get_variable("weights", kernel_shape,
                               initializer=tf.random_normal_initializer());
     bias = tf.get_variable("bias", bias_shape,
@@ -14,12 +16,24 @@ def conv_relu(input, kernel_shape, bias_shape):
                         strides=[1, 1, 1, 1], padding="SAME");
     return tf.nn.relu(conv + bias);
 
-input1 = tf.random_normal([1,10,10,32]);
-input2 = tf.random_normal([1,20,20,32]);
-x = conv_relu(input1, kernel_shape=[5, 5, 32, 32], bias_shape=[32]);
+input_val1 = np.reshape(np.random.normal(0, 1, 3200),(1,10,10,32))
+#input_val2 = np.random_normal([1,20,20,32]);
+
+input_data = tf.placeholder(shape=([1,10,10,32]),dtype=tf.float32)
+#x = conv_relu(input_data, kernel_shape=[5, 5, 32, 32], bias_shape=[32]);
 
 def my_image_filter(input_images):
     with tf.variable_scope("conv1"):
         relu1 = conv_relu(input_images, [5, 5, 32, 32], [32]);
     with tf.variable_scope("conv2"):
-        return conv_relu(relu1, [5, 5, 32, 32], [32]);
+       return conv_relu(relu1, [5, 5, 32, 32], [32]);
+
+output = my_image_filter(input_data)
+
+with tf.Session() as sess:
+  sess.run(tf.global_variables_initializer())
+  output_val = sess.run(output, feed_dict={input_data: input_val1})
+  print(output_val)
+
+#my_image_filter(input1)
+#my_image_filter(input2)
